3627453a08d784c24557c2801916e5315d4e57d9
Aswin Gopakumar <28153287+aswingopakumar98@users.noreply.github.com>
2024-07-17 09:54:16 +0530

    form validated added.commented as of now

--- Frontend/urlshortnerappclient/src/app/home/home.component.ts
+++ Frontend/urlshortnerappclient/src/app/home/home.component.ts
@@ -1,35 +1,39 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup ,ReactiveFormsModule} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UrlshortenerService } from '../services/urlshortenerservice.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   urlForm!: FormGroup;
-  generatedUrl: string|undefined;
+  generatedUrl: string | undefined;
 
   constructor(private urlshortenerService: UrlshortenerService) {
 
   }
   ngOnInit() {
-    this.urlForm = new FormGroup({ longUrl: new FormControl() })
+    const reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+    this.urlForm = new FormGroup({ longUrl: new FormControl('', [Validators.required, Validators.pattern(reg)]) })
   }
   generateUrl() {
+
     console.log(this.urlForm)
-    var longUrl = this.urlForm.controls['longUrl'].value;
-    this.urlshortenerService.generateUrl(longUrl).subscribe({
-      next: (data) => {
-        console.log(data);
-        this.generatedUrl=data.generatedUrl;
-        
-      }, error: (error) => { console.error(error); }
-    }
-    );
+    // if (this.urlForm.valid) {
+      var longUrl = this.urlForm.controls['longUrl'].value;
+      this.urlshortenerService.generateUrl(longUrl).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.generatedUrl = data.generatedUrl;
+
+        }, error: (error) => { console.error(error); }
+      }
+      );
+    // }
   }
 }
